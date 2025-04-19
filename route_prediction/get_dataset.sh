6bec426d0af7da97571dc66965e6c9ac8e72d62e
Elllllllvin <100711005+Elllllllvin@users.noreply.github.com>
2025-04-19 17:15:09 +0800

    0419

---
+++ route_prediction/get_dataset.sh
@@ -0,0 +1,5 @@
+python get_dataset.py \
+    --fin_temp      "D:/githubClone/LaDe/route_prediction/data/tmp/pickup_cq/" \
+    --fin_original  "D:/githubClone/LaDe/route_prediction/data/raw/pickup/pickup_cq.csv" \
+    --data_name     "pickup_cq" \
+    --num_thread    8
